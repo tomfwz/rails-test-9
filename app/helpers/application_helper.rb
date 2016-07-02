3c5c7712574cfb98a8de48b64a7fae5f845311f6
Tom Futureworkz <tom@futureworkz.com>
2016-07-02 14:16:59 +0700

    Style comment partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def avatar(user)
     image_tag "http://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}"
   end
+
+  def current_user?(user)
+    user == current_user
+  end
 end
