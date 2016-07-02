52e74e877d86735fa4d76b050d72d1b7ebdc1231
Tom Futureworkz <tom@futureworkz.com>
2016-07-02 10:34:02 +0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+tom   = User.create!(name: 'Tom',    password: '123123123', email: 'tom@futureworkz.com')
+wayne = User.create!(name: 'Wayne',  password: '123123123', email: 'wayne@futureworkz.com')
+james = User.create!(name: 'James',  password: '123123123', email: 'james@futureworkz.com')
+
+
+initial_comment       = Comment.create!(    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: tom)
+first_tier_comments   = Comment.create!([{  message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: wayne, parent: initial_comment },             { message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: james, parent: initial_comment } ])
+second_tier_comments  = Comment.create!([{  message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: tom,   parent: first_tier_comments.first },   { message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: tom,   parent: first_tier_comments.second } ])
+third_tier_comments   = Comment.create!([{  message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: wayne, parent: second_tier_comments.first },  { message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user: james, parent: second_tier_comments.second } ])
